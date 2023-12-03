b8d88ab3f699d966113f6dbbef867bb2ce1d0957
Esa Kesti <esakesti@gmail.com>
2023-12-03 10:18:56 +0000

    commenting
    Added comments and a TODO

--- workTimeTracker.py
+++ workTimeTracker.py
@@ -1,13 +1,15 @@
+#Converts decimal time to normal time
 def convert_decimal_time(decima_time):
     hours = int(convert_decimal_time)
     minutes_decimal = (decima_time - hours) * 60
     minutes = round(minutes_decimal)
     return hours, minutes
 
+#initialise a workTime array
 workTime = []
-
-weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-
+#specify all workdays from Monday to Thursday
+weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday"]
+# Asks user to enter time worked 
 for weekday in weekdays:
     while True:
         try:
@@ -17,11 +19,14 @@ for weekday in weekdays:
             print("Invalid value, expecting a float")
     workTime.append(decimal_time_input)
 
+#TODO add a function to ask user what time they started working on Friday so that the go home time can be calculated
+
+#calls for the function that converts the decimal time
 converter = convert_decimal_time(workTime)
 normalConvert = sum(converter)
 work_days_total = sum(workTime)
 
-
+#prints out the values
 print(f"total hours worked: ", dict(zip(weekdays, workTime)))
 print(f"total hours worked: {work_days_total}")
 print(f"total normal hours worked: {normalConvert}")
